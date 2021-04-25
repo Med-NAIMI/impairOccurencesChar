5cc492a7037384799501f12171b532f29232f77f
Med-NAIMI <56475732+Med-NAIMI@users.noreply.github.com>
2021-04-25 19:47:59 +0200

    Add files via upload

---
+++ impairOccurencesChar/impairOccurencesChar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impairOccurencesChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("svp, entrez la chaine de charactere que vous voulez tester!");
+            string s=Console.ReadLine();
+            Console.WriteLine("svp, entrez le charactere que vous chercher!");
+            char c = (char) Console.Read();
+
+            //int n=Traitement.charOccurrences(s, c);
+
+            Console.WriteLine(Traitement.charOccurrences(s, c));
+            Console.ReadKey();//pour conserver l affichage
+
+        }
+    }
+}
---
+++ impairOccurencesChar/impairOccurencesChar/Traitement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impairOccurencesChar
+{
+    abstract class Traitement
+    {
+        public static int charOccurrences(string s, char c)
+        {
+            int i = 0;
+            //i l'indice du char dans le string, 
+            int nbrOccurrences = 0;//nbrOccurrences le nbr des occurences retrouvées
+            foreach(char c1 in s)
+            {
+                
+                if (i%2!=0 && c1==c)
+                {
+                    nbrOccurrences += 1;//on augmente de 1 lorsque les condition demandées sont satisfaites
+                }
+                i++;
+            }return nbrOccurrences;//on retourne le nbr d'occurrences
+            
+        }
+    }
+}
